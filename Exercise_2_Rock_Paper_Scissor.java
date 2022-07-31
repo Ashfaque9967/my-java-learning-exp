2b1dd10e37c15e591dd52901f821342c588f3753
Ashfaque <shaikhashfaque220@gmail.com>
2022-07-31 11:13:35 -0700

    my java learning files managed successfully

--- Exercise_2_Rock_Paper_Scissor.java
+++ Exercise_2_Rock_Paper_Scissor.java
@@ -1,45 +1,45 @@
-
-import java.util.Random;
-import java.util.Scanner;
-
-public class Exercise_2_Rock_Paper_Scissor {
-    public static void main(String[] args) {
-//        Enter 0 for Rock
-//        Enter 1 for Papergit log
-
-//        Enter 2 for Scissor
-//        Ignore this line
-        
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 0 for Rock ; Enter 1 for Paper ; Enter 2 for Scissor");
-        System.out.print("Enter your number here : ");
-        int userinput = sc.nextInt();
-        sc.close();
-        Random random = new Random();
-        int compinput = random.nextInt(3);
-
-//        Print of Computer choose
-        if (compinput == 0){
-            System.out.println("Computer choose: Rock");
-        }
-        else if (compinput == 1){
-            System.out.println("Computer choose: Paper");
-        }
-        else if (compinput == 2){
-            System.out.println("Computer choose: Scissor");
-        }
-
-        if (userinput == compinput){
-            System.out.println("Draw");
-        }
-        else if (userinput == 0 && compinput == 2 || userinput == 1 && compinput == 0 || userinput == 2 && compinput == 1) {
-            System.out.println("You Win!");
-        }
-        else {
-            System.out.println("You Loose!");
-        }
-    }
-}
-
-
+//
+//import java.util.Random;
+//import java.util.Scanner;
+//
+//public class Exercise_2_Rock_Paper_Scissor {
+//    public static void main(String[] args) {
+////        Enter 0 for Rock
+////        Enter 1 for Papergit log
+//
+////        Enter 2 for Scissor
+////        Ignore this line
+//
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter 0 for Rock ; Enter 1 for Paper ; Enter 2 for Scissor");
+//        System.out.print("Enter your number here : ");
+//        int userinput = sc.nextInt();
+//        sc.close();
+//        Random random = new Random();
+//        int compinput = random.nextInt(3);
+//
+////        Print of Computer choose
+//        if (compinput == 0){
+//            System.out.println("Computer choose: Rock");
+//        }
+//        else if (compinput == 1){
+//            System.out.println("Computer choose: Paper");
+//        }
+//        else if (compinput == 2){
+//            System.out.println("Computer choose: Scissor");
+//        }
+//
+//        if (userinput == compinput){
+//            System.out.println("Draw");
+//        }
+//        else if (userinput == 0 && compinput == 2 || userinput == 1 && compinput == 0 || userinput == 2 && compinput == 1) {
+//            System.out.println("You Win!");
+//        }
+//        else {
+//            System.out.println("You Loose!");
+//        }
+//    }
+//}
+//
+//
