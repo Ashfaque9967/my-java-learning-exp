851951ac3bb4bc6dee957b7f14bdc1f9111fff4d
Ashfaque <shaikhashfaque220@gmail.com>
2022-08-04 09:42:33 -0700

    update

---
+++ L_51_packages.java
@@ -0,0 +1,14 @@
+//import java.util.Scanner;
+//import java.util.*;
+//
+//public class L_51_packages {
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+////        java.util.Scanner sc = new java.util.Scanner(System.in);
+//        System.out.print("Enter your number here: ");
+//        int s = sc.nextInt();
+//        System.out.println("your number = " + s);
+//
+//    }
+//}
