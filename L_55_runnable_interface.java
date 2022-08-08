03c1ecba702330d14f009ddfb042caf477a10bcf
Ashfaque <shaikhashfaque220@gmail.com>
2022-08-08 10:12:28 -0700

    Implementing thread's Runnable Interfaces

--- L_55_runnable_interface.java
+++ L_55_runnable_interface.java
@@ -1,11 +1,15 @@
 //class thread1 implements Runnable{
 //    public void run(){
-//        System.out.println("I am Thread1's run...");
+//        while (true){
+//            System.out.println("I am Thread1's run...");
+//        }
 //    }
 //}
 //class thread2 implements Runnable{
 //    public void run(){
-//        System.out.println("I am Thread2's run...");
+//        while(true){
+//            System.out.println("I am Thread2's run...");
+//        }
 //    }
 //}
 //public class L_55_runnable_interface {
@@ -17,7 +21,7 @@
 //        Thread Gun1 = new Thread(bullet1);
 //        Gun1.start();
 //
-//        thread1 bullet2 = new thread1();
+//        thread2 bullet2 = new thread2();
 //        Thread Gun2 = new Thread(bullet2);
 //        Gun2.start();
 //
