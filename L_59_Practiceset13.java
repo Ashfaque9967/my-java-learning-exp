05ae0c6e99aacbb77b49dabe8f058634526d72ba
Ashfaque <shaikhashfaque220@gmail.com>
2022-08-10 11:11:04 -0700

    Practice set 13 on Threads

---
+++ L_59_Practiceset13.java
@@ -0,0 +1,61 @@
+//// Problem 1:
+//class mythr  extends Thread{
+//    public void run(){
+//        int i = 0;
+//        while(i<10){
+//
+////            Problem 2:
+//            try{
+//                Thread.sleep(4000);
+//            }catch (Exception e){
+//                System.out.println(e);
+//            }
+//
+//            System.out.println("Good Morning!");
+//            i++;
+//        }
+//    }
+//}
+//class mythr1  extends Thread{
+//    public void run(){
+//        int i = 0;
+//        while(i<15){
+//
+////            Problem 2:
+//            try{
+//                Thread.sleep(2000);
+//            }catch (Exception e){
+//                System.out.println(e);
+//            }
+//
+//            System.out.println("welcome!");
+//            i++;
+//        }
+//    }
+//}
+//public class L_59_Practiceset13 {
+//    public static void main(String[] args) {
+//
+////        Problem 1:
+//        mythr m = new mythr();
+//        m.start();
+//        mythr1 m1 = new mythr1();
+//        m1.start();
+//
+////        Problem 3:
+//        m.setPriority(8);
+//        m1.setPriority(8);
+//        System.out.println(m.getPriority());
+//        System.out.println(m1.getPriority());
+//
+////        Problem 4:
+//        System.out.println(m.getState());
+//        System.out.println(m1.getState());
+//
+//
+////        Problem 4:
+//        System.out.println(Thread.currentThread().getState());
+//
+//
+//    }
+//}
