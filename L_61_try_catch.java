335e1c137f54aa8c2df471d6a78c2dce99596239
Ashfaque <shaikhashfaque220@gmail.com>
2022-08-14 11:00:03 -0700

    Try catch method

---
+++ L_61_try_catch.java
@@ -0,0 +1,21 @@
+//public class L_61_try_catch {
+//    public static void main(String[] args) {
+//
+//        int a = 6000;
+//        int b = 9;
+//        int c;
+//
+//        // Without try catch
+//        // c= a/b;
+//
+//        // With try catch
+//        try{
+//            c= a/b;
+//            System.out.println("The value of c = " + c);
+//        }
+//        catch(Exception e){
+//            System.out.print("Failed : ");
+//            System.out.println(e);
+//        }
+//    }
+//}
