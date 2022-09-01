b3a32b6ab45d9ab5229e38307d792cbf54f9e8c1
Ashfaque <shaikhashfaque220@gmail.com>
2022-09-01 22:48:39 +0530

    LinkedList

---
+++ L_68_LinkedList.java
@@ -0,0 +1,29 @@
+//import java.util.LinkedList;
+//
+//public class L_68_LinkedList {
+//    public static void main(String[] args) {
+//        LinkedList<Integer> l1 = new LinkedList<>();
+//        l1.add(3);
+//        l1.add(4);
+//        l1.add(5);
+//        l1.add(2);
+//
+//        LinkedList<Integer> l2 = new LinkedList<>();
+//        l2.add(12);
+//        l2.add(34);
+//        l2.add(54);
+//        l2.addAll(0, l1);
+//        l2.addFirst(87);
+//        l2.addLast(94);
+//        System.out.println(l2.contains(87));
+//        System.out.println(l2.indexOf(5));
+//        System.out.println(l2.size()+"= Size of the array.\n");
+//        for (int inner:l1) {
+//            System.out.println(inner);
+//        }
+//        System.out.println();
+//        for (int i = 0;i< l2.size();i++){
+//            System.out.print(l2.get(i)+", ");
+//        }
+//    }
+//}
