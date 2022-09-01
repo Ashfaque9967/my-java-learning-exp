54ec2448bfdb978fa225e1a3f2250420f930551a
Ashfaque <shaikhashfaque220@gmail.com>
2022-09-02 00:00:30 +0530

    ArrayDeque

---
+++ L_68_ArrayDeque.java
@@ -0,0 +1,21 @@
+//import java.util.ArrayDeque;
+//
+//public class L_68_ArrayDeque {
+//    public static void main(String[] args) {
+//        ArrayDeque<String> s1 = new ArrayDeque<>();
+//        s1.add("Ashfaque");
+//        s1.add("Himesh");
+//        s1.addFirst("timmo");
+//        s1.add("shimmo");
+//        s1.add("Himmo");
+//        s1.pollLast();
+////        System.out.println(s1.peekFirst());
+////        System.out.println(s1.getLast());
+////        System.out.println(s1.getLast());
+//        int i = 0;
+//        for (String st:s1) {
+//            i++;
+//            System.out.println(i+". "+st);
+//        }
+//    }
+//}
