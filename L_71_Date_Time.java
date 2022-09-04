7df0882816dbd47516b43401d84b68485f6676ce
Ashfaque <shaikhashfaque220@gmail.com>
2022-09-04 11:39:52 +0530

    Date 'n' Time

---
+++ L_71_Date_Time.java
@@ -0,0 +1,8 @@
+//public class L_71_Date_Time {
+//    public static void main(String[] args) {
+//        System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+//
+////        Quick quiz
+//        
+//    }
+//}
