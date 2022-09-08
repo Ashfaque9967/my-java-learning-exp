279085dfb81bc6600a0003fbfbe4c2dc60def818
Ashfaque <shaikhashfaque220@gmail.com>
2022-09-09 00:11:16 +0530

    Time class & Date formatters

---
+++ L_75_Time_Class.java
@@ -0,0 +1,20 @@
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.time.LocalTime;
+//
+//public class L_75_Time_Class {
+//    public static void main(String[] args) {
+////        Date:
+//        LocalDate ld = LocalDate.now();
+//        System.out.println(ld);
+//
+////        Time:
+//        LocalTime lt = LocalTime.now();
+//        System.out.println(lt);
+//
+////        Date&Time:
+//        LocalDateTime ldt = LocalDateTime.now();
+//        System.out.println(ldt);
+//
+//    }
+//}
---
+++ L_76_DAteTimeFormatter.java
@@ -0,0 +1,27 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class L_76_DAteTimeFormatter {
+    public static void main(String[] args) {
+
+        LocalDateTime ldt = LocalDateTime.now();  // This is a date
+        System.out.println(ldt);
+        System.out.println("-------------------------------");
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");  // This is a Format
+        String date = ldt.format(dtf);  //Creating date string using date and format
+        System.out.println(date + " <-custom format");
+        System.out.println("-------------------------------");
+
+        DateTimeFormatter dtf1 = DateTimeFormatter.ISO_LOCAL_DATE;  // This is a pattern format
+        String Date = ldt.format(dtf1);  //Creating date string using date and format
+        System.out.println(Date + "<-pattern format");
+        System.out.println("-------------------------------");
+
+        // Creating my owm Date and Time format using formatter
+        DateTimeFormatter mf = DateTimeFormatter.ofPattern("h:m\nE, d LLL");
+        String md = ldt.format(mf);
+        System.out.println("---My Phone's Date 'n' Time format---\n"+md);
+        System.out.println("-------------------------------");
+    }
+}
